72a9f3f4baf4c02b02d8583f14b6efa3e537c554
Luka Kozina <lukakozina6@gmail.com>
2022-04-30 20:22:55 +0200

    Update takeoff_and_land.py

--- examples/takeoff_and_land.py
+++ examples/takeoff_and_land.py
@@ -1,71 +1,63 @@
 # Script takeoff_and_land.py is used for simple takeoff to 10 meters and landing.
 
-from __future__ import print_function
-import os
-import sys
+#!/usr/bin/env python3
 import rospy
-import mavros
-import threading
-import time
 import mavros_msgs
-from std_msgs.msg import String
+from mavros_msgs.msg import State
 from mavros_msgs.srv import SetMode, CommandBool,  CommandTOL
 
 class Drone:
-	def __init__(self):
-		self.rate = 1
-		self.connected = False
-	
-	def connect(self, node: str, rate: int):
-    		rospy.init_node(node, anonymous=True)
-    		self.rate = rospy.Rate(rate)
-    		self.connected = True
-    		rospy.loginfo("Connected...")
+    def __init__(self):
+        self.state = State()
+        self.connected = False
 
-	def arm(self):
-		print("Arming...")
-		rospy.wait_for_service('/mavros/cmd/arming')
-		try:
-			armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
-			armService(True)
-		except rospy.ServiceException as e:
-			print ("Service arm call failed: %s" %e)	
- 
-	def takeoff(self):
-		print("Takeoff ...")
-		rospy.wait_for_service('/mavros/cmd/takeoff')
-		try:
-			takeoffService = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
-			response = takeoffService(altitude = 10, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
-			rospy.loginfo(response)
-		except rospy.ServiceException as e:
-			print ("Service takeoff call failed: %s"%e)
-			
-	def land(self):
-		print("Landing... ")
-		rospy.wait_for_service('/mavros/cmd/land')
-		try:
-			landService = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
-			response = landService(altitude = 0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
-			rospy.loginfo(response)
-		except rospy.ServiceException as e:
-			print ("service land call failed: %s. The vehicle cannot land "%e) 
-	
-def main(args):
-	v=Drone()
-	v.connect("drone",rate=10)
-	time.sleep(10)
-	rospy.wait_for_service('/mavros/set_mode')
-	change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
-	response = change_mode(custom_mode="GUIDED")
-	print("Mode set to GUIDED")
-	time.sleep(10)
-	v.arm()
-	time.sleep(5)
-	v.takeoff()
-	time.sleep(10)
-	v.land()
-	time.sleep(10)
-	
+
+    def connect(self):
+        rospy.init_node('flying', anonymous=True)
+        self.state.connected==True 
+        rospy.loginfo("Connected...")
+
+    def arm_and_takeoff(self):
+        rospy.loginfo("Arming...")
+        rospy.wait_for_service('/mavros/cmd/arming')
+        try:
+            armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+            armService(True)
+            rospy.loginfo("Drone armed")
+        except rospy.ServiceException as e:
+            rospy.loginfo("Arming failed")
+
+        rospy.wait_for_service('/mavros/cmd/takeoff')
+        try:
+            takeoff = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
+            response = takeoff(altitude = 10, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+            rospy.loginfo("Takeoff ...")
+        except rospy.ServiceException as e:
+            rospy.loginfo("Takeoff failed")
+
+    def land(self):
+        rospy.wait_for_service('/mavros/cmd/land')
+        try:
+            landing = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
+            response = landing(altitude = 0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+            rospy.loginfo("Landing... ")
+        except rospy.ServiceException as e:
+            rospy.loginfo("Landing failed")
+
+
+def main():
+    v=Drone()
+    v.connect()
+    rospy.sleep(3)
+    rospy.wait_for_service('/mavros/set_mode')
+    change_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
+    response = change_mode(custom_mode="GUIDED")
+    v.arm_and_takeoff()
+    rospy.sleep(10)
+    v.land()    
+
 if __name__ == "__main__":
-	main(sys.argv) 
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
